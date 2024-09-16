5fb8fb4bb54dd713be55b86efbe71cb5526ea52f
DaniloBorreros <daniloborrerosjr@gmail.com>
2024-09-16 10:56:07 +0800

    for production

--- src/_lib/prisma.ts
+++ src/_lib/prisma.ts
@@ -1,7 +1,14 @@
-import { PrismaClient } from "@prisma/client"
- 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
- 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
- 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
+  }
+  prisma = (global as any).prisma;
+}
+
+export { prisma };
